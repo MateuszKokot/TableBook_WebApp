f293690ffa9b00c33d2480736dcaa8aa659fdd53
kolanowskim <83921557+kolanowskim@users.noreply.github.com>
2021-06-21 19:51:06 +0200

    Add files via upload

---
+++ ClientApp/src/views/KontaktView/KontaktView/KontaktView.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./KontaktView.module.scss";
+import ContactForm from "../../components/ContactForm/ContactForm";
+
+const KontaktView = () => {
+  return <ContactForm />;
+};
+
+export default KontaktView;
