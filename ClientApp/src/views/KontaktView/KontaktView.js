111541dbe0d1a084fa2d1c335e13b8ee83b0e6f0
kolanowskim <83921557+kolanowskim@users.noreply.github.com>
2021-06-21 19:51:37 +0200

    Delete KontaktView.js

--- ClientApp/src/views/KontaktView/KontaktView.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./KontaktView.module.scss";
-import ContactForm from "../../components/ContactForm/ContactForm";
-
-const KontaktView = () => {
-  return <ContactForm />;
-};
-
-export default KontaktView;
